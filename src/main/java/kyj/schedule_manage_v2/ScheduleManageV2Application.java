7bd251380b542cdfee238ee1f65422a07f1c5f41
KYJ <nananan1213@gmail.com>
2026-01-08 10:34:41 +0900

    feat: git init, 초기 스프링 설정

---
+++ src/main/java/kyj/schedule_manage_v2/ScheduleManageV2Application.java
@@ -0,0 +1,13 @@
+package kyj.schedule_manage_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManageV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManageV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kyj/schedule_manage_v2/ScheduleManageV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kyj.schedule_manage_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManageV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
